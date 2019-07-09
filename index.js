d92a60243fcd939aa31291d882a27b88243a3bcf
Randy Truong <truongrandy@gmail.com>
2019-07-09 07:48:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
-function shout(string) {
+function shout(string){
   return string.toUpperCase()
+};
+
+ shout("hello");
+
+ function logShout(string){
+  return console.log(string.toUpperCase())
 }
 
-function whisper(string) {
-  return string.toLowerCase()
+ logShout("hello")
+
+ function whisper(string){
+  return string.toLowerCase();
 }
 
-function logShout(string){
-  console.log(string.toUpperCase())
+ whisper("HELLO")
+
+ function logWhisper(string){
+  return console.log(string.toLowerCase())
 }
 
-function logWhisper(string){
-  console.log(string.toLowerCase())
+ logWhisper("HELLO")
+
+ function sayHiToGrandma(string){
+  if (string === string.toLowerCase()) return "I can't hear you!";
+  else if (string === "I love you, Grandma.") return "I love you, too.";
+  else if (string === string.toUpperCase()) return "YES INDEED!";
 }
 
-
-function sayHiToGrandma(string){
-  var uppercase = "HELLO"
-  var lowercase = "hello"
-
-  if (string === lowercase){
-  if (string === string.toLowerCase()){
-    return "I can't hear you!"
-  }else if (string === uppercase){
-  }else if (string === string.toUpperCase()){
-    return "YES INDEED!"
-  }else if (string === "I love you, Grandma."){
-    return "I love you, too."
-    }
-  }
-}
+ sayHiToGrandma("hello");
+sayHiToGrandma("HELLO");
+sayHiToGrandma("I love you, Grandma.");
